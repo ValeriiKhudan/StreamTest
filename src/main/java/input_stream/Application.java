1e1e737a8dcdda9e197ffa1a64706dd4b06d0995
ValeriiKhudan <1860lucky1860@gmail.com>
2016-05-29 11:40:45 +0300

    test v.0.3a

--- src/main/java/input_stream/Application.java
+++ src/main/java/input_stream/Application.java
@@ -3,12 +3,13 @@ package input_stream;
 
 public class Application {
     public static String decrypt(String fileName, String password){
-        try{
-            Cipher aes = getInstance("AES");
-            byte[] pass = MessageDigest.getInstance("MD5").digest(password.getBytes());
-            Key key = new SecretKeySpec(pass, "AES");
-
-            aes.
-        }
+//        try{
+//            Cipher aes = getInstance("AES");
+//            byte[] pass = MessageDigest.getInstance("MD5").digest(password.getBytes());
+//            Key key = new SecretKeySpec(pass, "AES");
+//
+//            aes.
+//        }
+        return null;
     }
 }
